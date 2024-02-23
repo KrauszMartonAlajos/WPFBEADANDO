24471c4d6b619e080b65eae62265db47a2c75bfb
KrauszMartonAlajos <77116668+KrauszMartonAlajos@users.noreply.github.com>
2024-02-23 01:29:53 -0700

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFASZverés
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Button gomb = new Button();
+            gomb.Width = 200;
+            gomb.Height = 32;
+            gomb.Content = "new gomb()";
+            GridMain.Children.Add(gomb);
+        }
+    }
+}
